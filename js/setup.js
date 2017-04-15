941a2b3409cbb575e88cd3bb18c6bd478b8cc7f2
Boris Osechinskiy <osechinskiy@gmail.com>
2017-04-15 03:42:31 +0300

    Сделал 1 задачу

--- js/setup.js
+++ js/setup.js
@@ -49,3 +49,65 @@ var similarListElement = document.querySelector('.setup-similar-list');
 similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = setup.querySelector('.setup-close');
+var setupSubmit = setup.querySelector('.setup-submit');
+var setupUserNname = setup.querySelector('.setup-user-name');
+
+var onModalWindowOpen = function(){
+setup.classList.remove('hidden');
+};
+var onModalWindowClose = function(){
+setup.classList.add('hidden');
+};
+var onModalWindowEscPress = function(evt) {
+  if (evt.keyCode === 27) {
+    onModalWindowClose();
+  }
+};
+setupOpen.addEventListener('click', function() {
+  onModalWindowOpen();
+  document.addEventListener('keydown', onModalWindowEscPress)
+});
+setupOpen.addEventListener('keydown', function(evt) {
+if (evt.keyCode === 13) {
+  onModalWindowOpen();
+}
+});
+
+setupClose.addEventListener('click', function() {
+  setup.classList.add('hidden');
+  });
+
+
+setupClose.addEventListener('keydown', function(evt) {
+if (evt.keyCode === 13) {
+  onModalWindowClose()
+}
+});
+setupUserNname.removeEventListener('keydown', onModalWindowEscPress);
+
+
+  setupSubmit.addEventListener('click', function() {
+    if (setupSubmit.valid) {
+    onModalWindowClose();
+  }
+  });
+
+  setupSubmit.addEventListener('keydown', function(evt) {
+  if (setupSubmit.valid & evt.keyCode === 13) {
+    onModalWindowClose();
+    }
+  });
+
+
+// setupSubmit.addEventListener('click', function() {
+//   setup.classList.add('hidden');
+// setupSubmit.addEventListener('keydown', function(evt) {
+// if (evt.keyCode === 13) {
+//   setup.classList.add('hidden');
+//   }
+// });
+// });
